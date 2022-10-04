cd263b5edee40968e1f3190df0750b7c75e5e0d3
Papiexx <boutdepapier27@outlook.fr>
2022-10-04 07:52:59 +0200

    add contract model + admin

--- Epic_Event_APP/contracts/admin.py
+++ Epic_Event_APP/contracts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contract
+
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = (
+        'sales_contact_id',
+        'customer_id',
+        'date_created',
+        'date_updated',
+        'status',
+        'amount',
+        'payment_due',
+    )
+
+
+admin.site.register(Contract, ContractAdmin)
--- Epic_Event_APP/contracts/models.py
+++ Epic_Event_APP/contracts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from customers.models import Customer
+
+
+class Contract(models.Model):
+    """"""
+    sales_contact_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    amount = models.FloatField(blank=False)
+    payment_due = models.DateTimeField(blank=False)
