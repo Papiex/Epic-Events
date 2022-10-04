5c6f437afbb6e64e0471988c38f3c135dbd0226a
Papiexx <boutdepapier27@outlook.fr>
2022-10-04 07:54:19 +0200

    adding event model + admin

--- Epic_Event_APP/events/admin.py
+++ Epic_Event_APP/events/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'customer_id',
+        'date_created',
+        'date_updated',
+        'support_contact_id',
+        'event_statut_id',
+        'attendees',
+        'event_date',
+        'notes',
+        'contract_id',
+        )
+
+
+admin.site.register(Event, EventAdmin)
--- Epic_Event_APP/events/models.py
+++ Epic_Event_APP/events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from customers.models import Customer
+from contracts.models import Contract
+
+class Event(models.Model):
+    """"""
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    support_contact_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    event_statut_id = models.BooleanField(default=False, blank=False)
+    attendees = models.IntegerField(blank=False)
+    event_date = models.DateTimeField(blank=False)
+    notes = models.TextField(max_length=250)
+    contract_id = models.OneToOneField(Contract, on_delete=models.CASCADE, blank=False)
